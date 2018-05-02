f03c82373d70cdae74336a4a0f0f76a1f2d7ab65
tqq <2521000359@qq.com>
2018-05-02 13:20:06 +0800

    search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="search">
+    <input type="text" placeholder="输入城市名或拼音" />
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    back () {
+      this.$router.back()
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .search
+    height: .72rem
+    background: $bgColor
+    padding: 0 .1rem
+    input
+      box-sizing: border-box
+      height: .62rem
+      padding: 0 .1rem
+      line-height: .62rem
+      width: 100%
+      text-align: center
+      border-radius: .06rem
+      color: #666
+</style>
